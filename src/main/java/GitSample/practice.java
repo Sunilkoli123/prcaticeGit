9a3e26d8aa7ffdb9364086403d381eede38c9419
Sunil <sunilyk143@gmail.com>
2024-01-18 08:13:19 +0530

    commit and push

---
+++ src/main/java/GitSample/practice.java
@@ -0,0 +1,8 @@
+package GitSample;
+
+public class practice {
+public static void main(String[] args) {
+	System.out.println("hello.......!");
+}
+}
+//https://github.com/Sunilkoli123/prcaticeGit.git
