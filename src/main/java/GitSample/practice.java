b84d95bade15a6b72a2a9e16384864c71a389a53
91733 <91733@sunil>
2024-01-19 08:10:08 +0530

    Login page is added

--- src/main/java/GitSample/practice.java
+++ src/main/java/GitSample/practice.java
@@ -3,6 +3,7 @@ package GitSample;
 public class practice {
 public static void main(String[] args) {
 	System.out.println("hello.......!");
+	System.out.println("hi sunilllll");
 }
 }
 //https://github.com/Sunilkoli123/prcaticeGit.git
---
+++ src/main/java/Gitsample1/LoginPage.java
@@ -0,0 +1,7 @@
+package Gitsample1;
+
+public class LoginPage {
+public static void main(String[] args) {
+	System.out.println("pusehd login page");
+}
+}
